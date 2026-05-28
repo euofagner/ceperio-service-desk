99fad97abce2bb6af6d82dec8abe5c0b67f3b4a1
Fagner <fagnergeo0@gmail.com>
2026-05-27 21:49:48 -0300

    add scalar for endpoints documentation

--- backend/CeperioServiceDesk/CeperioServiceDesk.API/Data/AppDbContext.cs
+++ backend/CeperioServiceDesk/CeperioServiceDesk.API/Data/AppDbContext.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CeperioServiceDesk.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CeperioServiceDesk.API.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 { 
+    public DbSet<Ticket> Tickets { get; set; }
 }
