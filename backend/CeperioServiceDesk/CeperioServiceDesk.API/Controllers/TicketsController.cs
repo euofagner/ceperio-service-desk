4bcdbabdc10b59aaf86a57a3b2d87346a7c90420
Fagner <fagnergeo0@gmail.com>
2026-06-02 07:14:50 -0300

    add POST endpoint for ticket creation

---
+++ backend/CeperioServiceDesk/CeperioServiceDesk.API/Controllers/TicketsController.cs
@@ -0,0 +1,32 @@
+﻿using CeperioServiceDesk.API.Data;
+using CeperioServiceDesk.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CeperioServiceDesk.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TicketsController(AppDbContext context) : ControllerBase
+{
+    private readonly AppDbContext _context = context;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Ticket>>> GetTickets()
+    {
+        var tickets = await _context.Tickets.ToListAsync();
+        return Ok(tickets);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> PostTicket(Ticket ticket)
+    {
+        if (ticket is null) return BadRequest();
+
+        await _context.Tickets.AddAsync(ticket);
+        await _context.SaveChangesAsync();
+
+        return Created();
+    }
+}
