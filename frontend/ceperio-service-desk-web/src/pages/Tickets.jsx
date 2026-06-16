42abb1e32267bdca741ee1fee820656a2f7436e6
Fagner <fagnergeo0@gmail.com>
2026-06-16 20:45:51 -0300

    show tickets in frontend from api

--- frontend/ceperio-service-desk-web/src/pages/Tickets.jsx
+++ frontend/ceperio-service-desk-web/src/pages/Tickets.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function Tickets() {
 
+    const [tickets, setTickets] = useState([]);
+
+    async function getTickets() {
+        const ticketsFromApi = await api.get("/tickets");
+
+        setTickets(ticketsFromApi.data);
+    }
+
+    useEffect(() => {
+        getTickets();
+    }, []);
+
     return (
-        <div className="min-h-screen bg-gray-300 p-8">
-            <h1>Ceperio Service Desk</h1>
+        <div className="min-h-screen text-gray-200 bg-neutral-900 p-8">
+            <h1>Tickets</h1>
+
+            <div>
+                {tickets.map(ticket =>
+                    <div key={ticket.id}>
+                        <br />
+                        <h2>{ticket.title}</h2>
+                        <p>{ticket.description}</p>
+                        <span>
+                            Status: {ticket.ticketStatus} | Prioridade: {ticket.ticketPriority}
+                        </span>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
