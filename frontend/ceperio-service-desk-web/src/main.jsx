fb276505cc2a6f607b1afc3f2d0e01bce7d5442d
Fagner <fagnergeo0@gmail.com>
2026-08-29 19:28:29 -0300

    feat(front): wrap app with AuthProvider

--- frontend/ceperio-service-desk-web/src/main.jsx
+++ frontend/ceperio-service-desk-web/src/main.jsx
@@ -1,11 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import './index.css'
-import { router } from './routes/AppRoutes'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './routes/AppRoutes';
+import { AuthProvider } from "./contexts/AuthContext";
+
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
+    </React.StrictMode>
+);
