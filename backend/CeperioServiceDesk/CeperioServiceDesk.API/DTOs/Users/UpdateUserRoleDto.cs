311ddd39d4d07d0cddf0765f85613bb05e438814
Fagner <fagnergeo0@gmail.com>
2026-09-01 11:16:09 -0300

    feat(backend): add user DTOs for role update and responseDTO

---
+++ backend/CeperioServiceDesk/CeperioServiceDesk.API/DTOs/Users/UpdateUserRoleDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CeperioServiceDesk.API.DTOs.Users;
+
+public class UpdateUserRoleDto
+{
+    [Required]
+    public string Role { get; private set; } = string.Empty;
+}
